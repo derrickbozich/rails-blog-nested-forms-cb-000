21482c78e3873107b52cc1916658a9b2f236839b
derrick b <dbozich1@gmail.com>
2018-08-26 05:41:28 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ActiveRecord::Base
 
   def tags_attributes=(tags_hashes)
     tags_hashes.each do |i, tag_attributes|
-      tag = Tag.create_or_find_by(name: tag_attributes[:name])
+      tag = Tag.all.create_or_find_by(name: tag_attributes[:name])
       self.post_tags.build(:tag => tag)
     end
   end
