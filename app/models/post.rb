9421d51196afb83370eebe914a0b4d8052517c69
derrick b <dbozich1@gmail.com>
2018-08-26 05:37:32 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,17 +8,17 @@ class Post < ActiveRecord::Base
 
   validates_presence_of :name, :content
 
-  def tags_attributes=(tags_hashes)
-    tags_hashes.each do |tag_attributes|
-      # if tag_attributes[:name].present?
-        tag = Tag.create_or_find_by(:name => tag_attributes[:name])
-        # if !self.tags.include?(tag)
-          self.post_tags.build(:tag => tag)
-        # end
-
-      # end
-
-
-    end
-  end
+  # def tags_attributes=(tags_hashes)
+  #   tags_hashes.each do |tag_attributes|
+  #     # if tag_attributes[:name].present?
+  #       tag = Tag.create_or_find_by(:name => tag_attributes[:name])
+  #       # if !self.tags.include?(tag)
+  #         self.post_tags.build(:tag => tag)
+  #       # end
+  #
+  #     # end
+  #
+  #
+  #   end
+  # end
 end
